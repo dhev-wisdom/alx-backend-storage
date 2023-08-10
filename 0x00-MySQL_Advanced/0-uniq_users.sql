c6ec66e1fba8f6d88bbc1afd902c112bacd832c8
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-10 21:38:28 +0100

    First advanced mysql code

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create table 'users' with attributes id, email and name
+-- Script should be executable on any database
+CREATE TABLE users IF NOT EXISTS (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
