fa2f46d9c8b2b2fa4071f8c38f9ed62e529fca13
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 15:20:18 +0100

    check 9

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,6 @@
+-- SQL script creates an index 'idx_name_first'
+-- on the table names and the first letter of name
+DELIMITER $$
+CREATE INDEX idx_name_first_score ON names (name(1), score)
+$$
+DELIMITER ;
