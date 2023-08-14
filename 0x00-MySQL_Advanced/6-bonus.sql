c027e79147424d89688fdd32ee796cdbec29e71f
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 14:46:40 +0100

    check again

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -4,14 +4,14 @@ DELIMITER $$
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
 	DECLARE project_id INT;
-	IF ((SELECT COUNT(*) FROM projects WHERE name = project_name) < 1) THEN
-		INSERT INTO projects (name) VALUES(project_name);
+	IF ((SELECT COUNT(*) FROM projects WHERE name = project_name) = 0) THEN
+		INSERT INTO projects (name) VALUES (project_name);
 		SET project_id = LAST_INSERT_ID();
 	ELSE
 		SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1;)
-	END IF
+	END IF;
 	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
 END
 $$
-DELIMITER;
+DELIMITER ;
