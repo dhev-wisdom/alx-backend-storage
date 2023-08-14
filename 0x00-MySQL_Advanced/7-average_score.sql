37f1b8c008c36c7e07eb17bf7a4d032c95f5f0ce
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 14:41:36 +0100

    Check

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,31 +4,9 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE score INT;
-	DECLARE total_scores INT;
-	DECLARE sum_scores INT;
-	DECLARE average FLOAT;
-
-	DECLARE cur CURSOR FOR
-		SELECT score FROM corrections WHERE user_id = user_id;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET total_scores = 0;
-
-	SET total_scores = 0;
-	SET sum_scores = 0;
-
-	OPEN cur;
-	read_loop: LOOP
-		FETCH cur INTO score;
-		SET sum_scores = sum_scores + score;
-		SET total_scores = total_scores + 1;
-	END LOOP read_loop;
-	CLOSE cur;
-
-	SET average = sum_scores / total_scores;
-
 	UPDATE users
-	SET average_score = average
-	WHERE id = user_id;
+	SET average_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id = user_id);
+	WHERE users.id = user_id;
 END
 $$
 DELIMITER ;
