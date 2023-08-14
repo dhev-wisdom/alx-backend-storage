fda9fd02e8a8f344668696381f9db22406f6c3a8
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 23:54:24 +0100

    Chek

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -8,8 +8,8 @@ BEGIN
 	SET weighted_avg = (SELECT SUM(score * weight) / SUM(weight)
 		FROM users AS U
 		JOIN corrections AS C ON U.id = C.user_id
-		JOIN projects AS P ON P.id = C.project_id
-		WHERE U.id = user_id;
+		JOIN projects AS P ON C.project_id = P.id
+		WHERE U.id = user_id);
 	UPDATE users
 	SET average_score = weighted_avg WHERE id = user_id;
 END
