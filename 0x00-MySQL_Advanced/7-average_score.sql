3931eda0dd189236ff2e2e96fd31570e29d4180b
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 11:48:01 +0100

    Check

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,4 +3,31 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
+	DECLARE score INT;
+	DECLARE total_scores INT;
+	DECLARE sum_scores INT;
+	DECLARE average FLOAT;
 
+	DECLARE cur CURSOR FOR
+		SELECT score FROM corrections WHERE user_id = user_id;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET total_scores = 0;
+
+	SET total_scores = 0;
+	SET sum_scores = 0;
+
+	OPEN cur;
+	read_loop: LOOP
+		FETCH cur INTO score;
+		SET sum_scores = sum_scores + score;
+		SET total_scores = total_scores + 1;
+	END LOOP;
+	CLOSE cur;
+
+	SET average = sum_scores / total_scores;
+
+	UPDATE users
+	SET average_score = average
+	WHERE id = user_id;
+END
+$$
+DELIMITER ;
