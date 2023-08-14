9b1701b9e01337f039fbcf87136c0c4b9ffa9f52
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-14 16:57:25 +0100

    check

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,6 +6,6 @@ $$
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE score <= 80
-AND (last_meeting IS NULL OR last_meeting > DATE_ADD(NOW(), INTERVAL 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE_ADD(NOW(), INTERVAL 1 MONTH));
 $$
 DELIMITER ;
