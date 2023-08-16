ab36a5863be6bac78cbbc58f98f624132055daa8
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-16 22:47:48 +0100

    exercise.py update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Introduction to Redis database
 
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 
 class Cache():
@@ -27,3 +27,26 @@ class Cache():
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        custom get method
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return None
+        return fn(value)
+
+    def get_str(self, key: str) -> str:
+        """
+        get_str method
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        get_int method
+        """
+        return self.get(key, fn=int)
