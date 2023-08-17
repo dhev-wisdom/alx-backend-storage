6c5b9ff31670b742364d7be2c2f3e5bb16a48c34
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-17 23:43:52 +0100

    check 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,5 +8,5 @@ def schools_by_topic(mongo_collection, topic):
     """
     function returns the list of school having a specific topic:
     """
-    cur = mongo_collection.find({"topic": topic})
+    cur = mongo_collection.find({"topics": topic})
     return list(cur)
