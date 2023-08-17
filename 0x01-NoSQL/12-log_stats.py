b53122435dcd45b2bc0ecc6a8375ed5594cb7d3e
dhev-wisdom <nonsowidom62@gmail.com>
2023-08-18 00:08:20 +0100

    regular expression

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module provides some stats about Nginx logs stored in MongoDB
+"""
+
+def main():
+    """
+    Function provides some stats about Nginx logs stored in MongoDB
+    """
+
